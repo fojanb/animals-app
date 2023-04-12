477cfafcec7451d85392b6baa8e1b5891def4890
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-04-12 13:10:18 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,4 @@
+const App = () => {
+  return <div>hey hey</div>;
+};
+export { App };
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {App} from "./App";
+let el = document.getElementById("root");
+const root = ReactDOM.createRoot(el)
+root.render(<App />);

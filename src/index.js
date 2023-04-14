0a275bf6131403d5b9f21f9a816fbc72a51b27af
fojanb <fozhanmail@gmail.com>
2023-04-13 21:25:31 -0700

    Adding imageSource array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {App} from "./container/App/App";
+import App from "./container/App/App";
 import "./index.css";
 let el = document.getElementById("root");
 const root = ReactDOM.createRoot(el)

17f8f09a1bb73d15355311a48ed998a67dffd70c
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-04-14 16:26:50 -0700

    State system and Event system

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,31 +1,14 @@
-import Button from "../../components/Button/Button";
-const imageSources = [
-  {
-    src: "https://user-images.githubusercontent.com/23070621/231611632-c7e21b11-0392-4145-a180-321734ddfd0f.png",
-    animalName: "Zebra",
-  },
-  {
-    src: "https://user-images.githubusercontent.com/23070621/231936978-9f017df8-6490-4816-b6c4-81082df5f09f.png",
-    animalName: "Rabit",
-  },
-  {
-    src: "https://user-images.githubusercontent.com/23070621/231936485-7eabca0a-1a73-49fd-a88f-fbb98935b174.png",
-    animalName: "Cow",
-  },
-
-];
-const clickHandler = () => {
-  const animal = document.createElement("img");
-  animal.classList.add("img_size");
-  animal.src =
-    "https://user-images.githubusercontent.com/23070621/231611632-c7e21b11-0392-4145-a180-321734ddfd0f.png";
-  document.body.append(animal);
-};
+import { useState } from "react";
 const App = () => {
+  const [counter, setCounter] = useState(0);
+  const clickHandler = () => {
+    setCounter(counter + 1);
+  };
   return (
-    <div>
-      <Button clickHandler={clickHandler} />
-    </div>
+    <>
+      <button onClick={clickHandler}>Add Animal</button>
+      <div>number of Animals :{counter}</div>
+    </>
   );
 };
 export default App;

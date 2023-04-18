e20f6793fdf307820a4a311db2c9fac3895287df
fojanb <fozhanmail@gmail.com>
2023-04-17 22:58:29 -0700

    Adding green heart icon next to each animal image

--- src/component/AnimalShow/AnimalShow.js
+++ src/component/AnimalShow/AnimalShow.js
@@ -1,11 +1,18 @@
+import "./AnimalShow.css";
 const AnimalShow = ({ src, alt }) => {
+  const clickHandler = (e) => {
+    e.target.style.fontSize = "2rem";
+  }
   return (
     <>
       <img
         src={src}
         alt={alt}
-        style={{ width: "100px", height: "100px",margin:"20px" }}
+        style={{ width: "100px", height: "100px", margin: "20px" }}
       />
+      <span id={alt} className="heart" onClick={e => clickHandler(e)}>
+        &#128154;
+      </span>
     </>
   );
 };
